801d4e355782a8746806c17f48e0798d2138bfbf
MVvdV <mike@vandevliet.eu>
2026-01-17 11:54:45 +1100

    added calculator sub-project

--- main.py
+++ main.py
@@ -20,18 +20,23 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
     response = client.models.generate_content(model=model, contents=messages)
+
     if isinstance(response, NoneType):
         raise RuntimeError("No responce from the API call")
-    print(
-        f"Prompt: {args.user_prompt}\n"
-        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
-        f"Response tokens: {response.usage_metadata.candidates_token_count}\n"
-        f"Response:\n{response.text}"
-    )
+
+    if args.verbose:
+        print(
+            f"User prompt: {args.user_prompt}\n"
+            f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+            f"Response tokens: {response.usage_metadata.candidates_token_count}\n"
+            f"Response:\n{response.text}"
+        )
+    else:
+        print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
